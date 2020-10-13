45869fb46ffd69bd205514bb9a722f8e15faa347
Marija <maria5stojanovic@gmail.com>
2020-10-13 09:52:17 -0400

    adding book to the list

---
+++ app.js
@@ -0,0 +1,46 @@
+/*jshint esversion: 6 */
+
+//Book constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+//UI constructor
+function UI() {}
+
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById("book-list");
+  const row = document.createElement("tr");
+  row.innerHTML = `
+<td>${book.title}</td>
+<td>${book.author}</td>
+<td>${book.isbn}</td>
+<td><a href="#" class= "delete">X</a></td>
+
+`;
+  list.appendChild(row);
+};
+UI.prototype.clearFields=function(){
+document.getElementById('title').value='';
+document.getElementById('author').value='';
+document.getElementById('isbn').value='';
+}
+
+//event listeners
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  //get form values
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+
+  const book = new Book(title, author, isbn);
+  const ui = new UI();
+
+  //add book to list
+  ui.addBookToList(book);
+  ui.clearFields();
+
+  e.preventDefault();
+});
